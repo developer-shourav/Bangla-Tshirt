bd1b086507d64e8ccf67a55c11038ec2072adc22
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-07 06:32:28 +0600

    home and heder component stracture added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home> </Home>
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}> </RouterProvider>
   </React.StrictMode>,
 )
