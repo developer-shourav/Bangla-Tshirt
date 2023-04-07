35936773368541df65d65ee2718749d72c1c5189
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-07 07:56:19 +0600

    t-shirt's data loading complete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const tShirts = useLoaderData();
+    console.log(tShirts);
     return (
         <div>
-            <h2>This is home</h2>
+            <h2>This is home </h2>
+            <h2>Total T-shirts{tShirts.length}</h2>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,8 @@ const router = createBrowserRouter([
     children: [
       {
        path:'/',
-       element:<Home> </Home>, 
+       element:<Home> </Home>,
+       loader: () => fetch('tShirts.json') 
       },
 
       {
