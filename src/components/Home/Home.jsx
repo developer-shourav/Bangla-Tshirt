f18d2e3abb90d22bebfc81903033872a7914b43a
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-08 12:36:12 +0600

    one items multiple added previent done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,46 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import TShirt from '../TShirt/TShirt';
-import Cart from '../Cart/Cart';
-import './Home.css';
-
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import TShirt from "../TShirt/TShirt";
+import Cart from "../Cart/Cart";
+import "./Home.css";
 
 const Home = () => {
-    const tShirts = useLoaderData();
-    const [cart, setCart] = useState([]);
+  const tShirts = useLoaderData();
+  const [cart, setCart] = useState([]);
 
-    const handleAddToCart = tShirt => {
-        const newCart = [...cart, tShirt];
-        setCart(newCart)
+  const handleAddToCart = (tShirt) => {
+    const isAdd = cart.find((product) => product?._id === tShirt?._id);
+    if (!isAdd) {
+      const newCart = [...cart, tShirt];
+      setCart(newCart);
     }
-
-    const handleRemoveFromCart = id => {
-        const cartAfterRemoveItem = cart?.filter( product => product?._id !== id);
-        setCart(cartAfterRemoveItem)
-
+    else{
+        alert(`Sorry! '${tShirt?.name}' is already added.`)
     }
-   
-    return (
+  };
 
-        <div className='home-container'>
+  const handleRemoveFromCart = (id) => {
+    const cartAfterRemoveItem = cart?.filter((product) => product?._id !== id);
+    setCart(cartAfterRemoveItem);
+  };
 
-            <div className="t-shirt-container">
-                {
-                    tShirts?.map( tShirt => <TShirt
-                    key={tShirt?._id}
-                    tShirt={tShirt}
-                    handleAddToCart={handleAddToCart}
-                    > </TShirt>)
-                }
-            </div>
+  return (
+    <div className="home-container">
+      <div className="t-shirt-container">
+        {tShirts?.map((tShirt) => (
+          <TShirt
+            key={tShirt?._id}
+            tShirt={tShirt}
+            handleAddToCart={handleAddToCart}
+          >
+            {" "}
+          </TShirt>
+        ))}
+      </div>
 
-            <div className="cart-container">
-                <Cart handleRemoveFromCart = {handleRemoveFromCart} cart={cart}> </Cart>
-              
-            </div>
-
-        </div>
-    );
+      <div className="cart-container">
+        <Cart handleRemoveFromCart={handleRemoveFromCart} cart={cart}>
+          {" "}
+        </Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
